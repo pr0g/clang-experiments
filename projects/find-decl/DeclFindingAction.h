35913d60b4cf1b32489dd2b4182e1d99262fa08d
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-04-04 18:41:36 +0100

    ran clang-tidy on project

--- projects/find-decl/DeclFindingAction.h
+++ projects/find-decl/DeclFindingAction.h
@@ -10,7 +10,7 @@
 class DeclFindingAction : public clang::ASTFrontendAction {
 public:
     std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
-        clang::CompilerInstance& ci, clang::StringRef) final {
+        clang::CompilerInstance& ci, clang::StringRef /*InFile*/) final {
         return std::unique_ptr<clang::ASTConsumer>(
             new DeclFinder(ci.getSourceManager())
         );
