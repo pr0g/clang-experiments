aa423cbe869806d82bc0df6bed8d278bd45a7345
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-04-04 21:29:50 +0100

    switch to msvc compiler for compatibility with vscode debugger

--- projects/vector-value/configure-debug.bat
+++ projects/vector-value/configure-debug.bat
@@ -1,8 +1,13 @@
 @echo off
 
-REM Ninja Build
-cmake -S . -B build-deb/ -G Ninja^
-    -DCMAKE_BUILD_TYPE=Debug^
-    -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/LLVM/Release/bin/clang-cl.exe"^
+REM MSVC
+cmake -S . -B build/ -G Ninja^
     -DCMAKE_PREFIX_PATH="C:/Program Files (x86)/LLVM/Debug/lib/cmake/clang;C:/Program Files (x86)/LLVM/Debug/lib/cmake/llvm"^
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+REM Clang
+REM cmake -S . -B build-deb/ -G Ninja^
+REM     -DCMAKE_BUILD_TYPE=Debug^
+REM     -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/LLVM/Release/bin/clang-cl.exe"^
+REM     -DCMAKE_PREFIX_PATH="C:/Program Files (x86)/LLVM/Debug/lib/cmake/clang;C:/Program Files (x86)/LLVM/Debug/lib/cmake/llvm"^
+REM     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
